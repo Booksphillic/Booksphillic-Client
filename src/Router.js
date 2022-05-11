264b1475f2db428ed9738c96fe5f28d20182a168
Sae-byeol <toquf0424@naver.com>
2022-05-11 17:49:57 +0900

    feat: 미스터리북 apply page

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import ApplyPickup from './pages/ApplyPickup';
 const Router = () => {
   return (
     
        
     <Routes>
-            <Route path="/" element={<Home/>}/>    
+            <Route path="/" element={<Home/>}/>   
+            <Route path="/apply" element={<ApplyPickup/>}></Route> 
     </Routes>
             
        
