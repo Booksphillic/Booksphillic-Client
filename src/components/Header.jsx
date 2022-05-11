e5ea4917281b7ed79450ecef8ada51d1fc0e69c7
Sae-byeol <toquf0424@naver.com>
2022-05-11 17:56:11 +0900

    feat: header ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import styled from "styled-components"
 import { BorderWhiteBtn } from './Buttons'
 import { RowContainer } from './Container'
-
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
       <HeaderContainer>
         <HeaderChildContainer>
-          <img src="../img/logo.png"></img>
+          <Img src="../img/logo.png"></Img>
           <Menu>동네컬렉션</Menu>
           <Menu>서점프로필</Menu>
         </HeaderChildContainer>
         <HeaderChildContainer>
-          <BorderWhiteBtn>미스터리북 신청하기</BorderWhiteBtn>
+          <BorderWhiteBtn>
+            <Link to="/apply">미스터리북 신청하기</Link></BorderWhiteBtn>
           <img src='../img/search.png'></img>
           <img src='../img/mypage.png'></img>
         </HeaderChildContainer>
@@ -27,6 +28,9 @@ const HeaderContainer=styled(RowContainer)`
   background-color: #2A3143;
   padding: 0px 44px;
   justify-content: space-between;
+`
+const Img=styled.img`
+  
 `
 const Menu=styled.div`
   color: white;
