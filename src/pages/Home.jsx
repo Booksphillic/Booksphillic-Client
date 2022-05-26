ec768eb7195dac74cdcfe8a873e51e2139ac334a
0standing2 <ysyoung1011@sookmyung.ac.kr>
2022-05-27 06:15:18 +0900

    chore: 오타 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -149,7 +149,7 @@ const Home = () => {
               {PostContent('컨텐츠 경쟁시대에 이르렀다. 컨텐츠의 원조, 책에 진심인 사람들과 책으로 둘러쌓인 공간에서 오감으로 책을 느껴보는 시간을 가져보자.')}
             </BookstoreContent>
 
-            <Link to="/collction">
+            <Link to="/collection">
               <MoreBtn>
               <div style={{marginRight: '12px'}}>더 많은 컨텐츠 <br/>보러가기</div>
               <img src='../img/arrow.png'></img>
